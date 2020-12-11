05229ea970d005fa65c29526f7c754424cb0e494
annabelleee <aenriquez@getty.edu>
2020-12-11 15:38:18 -0800

    updated package changes for v5.1

--- v5_demo/urls.py
+++ v5_demo/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from .views.auth import SignupView
 
 urlpatterns = [
-    url(r'^', include('arches.urls')),
+    url(r"^auth/signup$", SignupView.as_view(), name="signup"),
+    url(r"^", include("arches.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.SHOW_LANGUAGE_SWITCH is True:
+    urlpatterns = i18n_patterns(*urlpatterns)
